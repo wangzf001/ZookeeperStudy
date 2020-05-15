c6e87a2d54310b2f15e1afd8a52a30d6c20e04ec
hero12 <731432751@qq.com>
2020-05-15 18:35:24 +0800

    curator基本操作

--- src/main/java/com/wzf/zookeeperstudy/curator/CuratorEventDemo.java
+++ src/main/java/com/wzf/zookeeperstudy/curator/CuratorEventDemo.java
@@ -1,6 +1,9 @@
 package com.wzf.zookeeperstudy.curator;
 
 import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.zookeeper.CreateMode;
 
 /**
  * @author 王振方
@@ -8,11 +11,27 @@ import org.apache.curator.framework.CuratorFramework;
  */
 public class CuratorEventDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        /**
+         * 三种watcher 做节点的监听
+         * pathcache 监听子节点的增删改
+         * nodecache 监听节点的增删改
+         * treecache   pathcache+nodecache
+         */
         CuratorFramework curatorFramework = CuratorClientUtils.getInstance();
 
+        NodeCache nodeCache = new NodeCache(curatorFramework,"/c1",false);
+        nodeCache.start();
 
+        nodeCache.getListenable().addListener(new NodeCacheListener() {
+            @Override
+            public void nodeChanged() throws Exception {
+                System.out.println("节点变化后的结果："+nodeCache.getCurrentData().getData());
+            }
+        });
+
+        curatorFramework.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/event");
 
 
 
